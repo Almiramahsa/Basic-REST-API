903ba3513b405b6a5f2054a375454d4045558fac
Almira Mahsa <almiramahsa9@gmail.com>
2023-02-11 22:00:55 +0700

    creating dummy api create,read, update, delete

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,6 +1,14 @@
 const getAllUsers = (req, res) => {
+  const data = {
+    id: '1',
+    name: 'Almira Mahsa',
+    email: 'almiramahsa9@gmail.com',
+    address: 'Palu, Sulawesi Tengah',
+  };
+
   res.json({
     message: 'GET all users success',
+    data: data,
   });
 };
 const createNewUser = (req, res) => {
@@ -11,7 +19,30 @@ const createNewUser = (req, res) => {
   });
 };
 
+const updateDataUser = (req, res) => {
+  const { idUser } = req.params;
+  console.log('idUser :', idUser);
+  res.json({
+    message: 'UPDATE User Success',
+    data: req.body,
+  });
+};
+
+const deleteDataUser = (req, res) => {
+  const { idUser } = req.params;
+  res.json({
+    message: 'DELETE user success',
+    data: {
+      id: idUser,
+      name: 'Almira Mahsa',
+      email: 'almiramahsa9@gmai.com',
+      address: 'Palu, Sulawesi Tengah',
+    },
+  });
+};
 module.exports = {
   getAllUsers,
   createNewUser,
+  updateDataUser,
+  deleteDataUser,
 };
--- src/routes/users.js
+++ src/routes/users.js
@@ -3,7 +3,15 @@ const express = require('express');
 const userController = require('../controller/users');
 const router = express.Router();
 
+//CREATE - POST
+router.post('/', userController.createNewUser);
+
+//READ - GET
 router.get('/', userController.getAllUsers);
 
-router.post('/', userController.createNewUser);
+//UPDATE - PATCH
+router.patch('/:idUser', userController.updateDataUser);
+
+//DELETE _ DELETE
+router.delete('/:idUser', userController.deleteDataUser);
 module.exports = router;
