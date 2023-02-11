41b5a041ae3410cde082822f98131df8df8dd50f
Almira Mahsa <almiramahsa9@gmail.com>
2023-02-11 21:24:11 +0700

    middleware testing

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const userRoutes = require('./routes/users');
+
+const middlewareLogRequest = require('./middleware/logs');
+
+const app = express();
+
+app.use(middlewareLogRequest);
+
+app.use('/users', userRoutes);
+
+app.listen(4000, () => {
+  console.log('Server berhasil di running di port 4000');
+});
